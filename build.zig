ffc41d5e99cb4535d9c3bb94e7480c058e7078da
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-07 17:23:15 -0500

    rename generic 'webserver' to 'pscl-webserver'

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
-        .name = "webserver",
+        .name = "pscl-webserver",
         .root_module = b.createModule(.{
             .root_source_file = b.path("main.zig"),
             .target = b.graph.host,
