fa8ff1b340616c85126257bd6802621b32c129f3
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2026-05-12 21:11:30 -0400

    add more mime types

--- main.zig
+++ main.zig
@@ -277,5 +277,10 @@ fn mimeType(path: []const u8) []const u8 {
     if (std.mem.endsWith(u8, path, ".css")) return "text/css";
     if (std.mem.endsWith(u8, path, ".js")) return "application/javascript";
     if (std.mem.endsWith(u8, path, ".html")) return "text/html";
+    if (std.mem.endsWith(u8, path, ".webp")) return "image/webp";
+    if (std.mem.endsWith(u8, path, ".png")) return "image/png";
+    if (std.mem.endsWith(u8, path, ".jpg")) return "image/jpeg";
+    if (std.mem.endsWith(u8, path, ".svg")) return "image/svg+xml";
+    if (std.mem.endsWith(u8, path, ".ico")) return "image/x-icon";
     return "application/octet-stream";
 }
